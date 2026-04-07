81f03dbbe88ca233259ea60f44aa446c8f2894ea
scarr <scarr@10.247.145.235>
2026-04-07 19:17:56 +0200

    Añadidas clases jugador

---
+++ src/org/pmoo/packageescoba/Jugador.java
@@ -0,0 +1,26 @@
+package org.pmoo.packageescoba;
+
+public abstract class Jugador {
+	//atributos
+	private ListaCartasMano mano;
+	private ListaCartasBarridas barridas;
+	private int puntos;
+	protected  Jugador(int pPuntos) {
+		this.mano = new ListaCartasMano();
+		this.barridas = new ListaCartasBarridas();
+		this.puntos = pPuntos;
+	}
+	public void sumarPuntos(int pPuntos) {
+		this.puntos = this.puntos + pPuntos;
+	}
+	protected ListaCartasMano getMano() {
+		return this.mano;
+	}
+	protected ListaCartasBarridas getBarridas() {
+		return this.barridas;
+	}
+	protected int getPuntos() {
+		return this.puntos;
+	}
+	public abstract void jugarTurno();
+}
---
+++ src/org/pmoo/packageescoba/JugadorPersona.java
@@ -0,0 +1,16 @@
+package org.pmoo.packageescoba;
+
+public class JugadorPersona extends Jugador {
+
+	public JugadorPersona(int pPuntos) {
+		super(pPuntos);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void jugarTurno() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
