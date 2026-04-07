2d74534a5f9013a42c58e027c5cc461db4f0f26c
scarr <scarr@10.247.145.235>
2026-04-07 19:28:56 +0200

    añadida clase teclado

--- src/org/pmoo/packageescoba/Mesa.java
+++ src/org/pmoo/packageescoba/Mesa.java
@@ -1,5 +1,6 @@
 package org.pmoo.packageescoba;
-
+import java.util.ArrayList;
+import java.util.Iterator;
 public class Mesa {
 	// atributos
 	private ArrayList<Jugador> jugadores;
@@ -14,5 +15,8 @@ public class Mesa {
 	}
 	public void jugarPartida() {
 		// TODO PROGRAMA PRINCIPAL, AQUÍ OCURRE EL BUCLE DE JUEGO CON LOS TURNOS DE CADA JUGADOR Y EL RECUENTO FINAL, LA PARTIDA TERMINA CUANDO SE ACABAN LAS CARTAS DE LAS MANOS DE LOS JUGADORES Y SE ACABAN LAS CARTAS DEL MAZO	
+	}
+	private Iterator<Jugador> getIterator(){
+		return this.jugadores.iterator();
 	}
 }
---
+++ src/org/pmoo/packageescoba/Teclado.java
@@ -0,0 +1,9 @@
+package org.pmoo.packageescoba;
+
+public class Teclado {
+
+	public Teclado() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
