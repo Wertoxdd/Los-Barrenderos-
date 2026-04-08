b8e23121629598162cb3afcc8760d96b5d467599
Wertoxdd <atep9390@gmail.com>
2026-04-08 13:48:43 +0200

    Teclado y jugador actualizados

--- src/org/pmoo/packageescoba/Jugador.java
+++ src/org/pmoo/packageescoba/Jugador.java
@@ -5,22 +5,28 @@ public abstract class Jugador {
 	private ListaCartasMano mano;
 	private ListaCartasBarridas barridas;
 	private int puntos;
+	
 	protected  Jugador(int pPuntos) {
 		this.mano = new ListaCartasMano();
 		this.barridas = new ListaCartasBarridas();
 		this.puntos = pPuntos;
 	}
+	
 	public void sumarPuntos(int pPuntos) {
 		this.puntos = this.puntos + pPuntos;
 	}
+	
 	protected ListaCartasMano getMano() {
 		return this.mano;
 	}
+	
 	protected ListaCartasBarridas getBarridas() {
 		return this.barridas;
 	}
+	
 	protected int getPuntos() {
 		return this.puntos;
 	}
+	
 	public abstract void jugarTurno();
 }
--- src/org/pmoo/packageescoba/Teclado.java
+++ src/org/pmoo/packageescoba/Teclado.java
@@ -1,9 +1,20 @@
 package org.pmoo.packageescoba;
 
+import java.util.Scanner;
+
 public class Teclado {
-
-	public Teclado() {
-		// TODO Auto-generated constructor stub
+	// objeto para leer inputs
+	private static Scanner sc = new Scanner(System.in);
+	
+	// metodos
+	
+	
+	public static int leerEntero(String mensaje) { // se pone un mensaje como parametro porque no sabemos en que caso se va a usar
+		System.out.println(mensaje);
+		return sc.nextInt();
 	}
-
+	
+	
+	
+	
 }
