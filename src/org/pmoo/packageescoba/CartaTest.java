21dd22efa54871f13f1d932cfa874536e332b286
Wertoxdd <atep9390@gmail.com>
2026-04-10 12:58:29 +0200

    CartaTest creado otra vez

---
+++ src/org/pmoo/packageescoba/CartaTest.java
@@ -0,0 +1,29 @@
+package org.pmoo.packageescoba;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CartaTest {
+
+	@Test
+	public void testCarta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPalo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetValor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
