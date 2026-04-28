a25fc3f15be425d528785b0892e60620a02bd1ad
Wertoxdd <atep9390@gmail.com>
2026-04-28 09:11:08 +0200

    ** COMENTARIOS **

--- src/org/pmoo/packageescoba/Main.java
+++ src/org/pmoo/packageescoba/Main.java
@@ -1,16 +1,7 @@
 package org.pmoo.packageescoba;
 
-/**
- * Clase principal que contiene el punto de entrada de la aplicación.
- */
 public class Main {
 	
-	/**
-	 * Método principal que inicia la ejecución del juego.
-	 * Crea la mesa e inicia la partida.
-	 * 
-	 * @param args Argumentos de la línea de comandos (no utilizados).
-	 */
 	public static void main(String[] args) {
 		Mesa.getMesa().jugarPartida();
 	}
